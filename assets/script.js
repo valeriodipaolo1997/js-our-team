c6e14754a8ff7564003a4c70b35a8cc2f7f57115
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-09-06 15:41:33 +0200

    add milestone 1

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
 /* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
 MILESTONE 1:
 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 MILESTONE 2:
@@ -10,3 +8,45 @@ Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede  */
 
+
+
+/*MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.*/
+
+const teamMembers = [
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        img: './assets/img/wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        name: 'Angela Caroll',
+        job: 'Chief Editor',
+        img: './assets/img/angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        name: 'Walter Gordon',
+        job: 'Office Manager',
+        img: './assets/img/walter-gordon-office-manager.jpg'
+    },
+
+    {
+        name: 'Angela Lopez',
+        job: 'Social Media Manager',
+        img: './assets/img/angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        img: './assets/img/scott-estrada-developer.jpg'
+    },
+
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        img: './assets/img/barbara-ramos-graphic-designer.jpg'
+    }
+]
